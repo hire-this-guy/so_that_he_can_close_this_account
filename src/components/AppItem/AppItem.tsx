acca45e13e3d2c384a1bb31a091bd224ff39775b
Krzyszof Lityński <klitynski@gog.com>
2021-06-04 14:14:22 +0200

    Prevent jumping when loading images

--- src/components/AppItem/AppItem.tsx
+++ src/components/AppItem/AppItem.tsx
@@ -10,7 +10,9 @@ const AppItem: React.FC<AppItemProps> = ({ app }) => {
 	return (
 		// TODO single place for lin generation?
 		<Link to={`/app/${app.id}`} className="AppItem">
-			<img src={app.iconURL} alt={app.name} className="AppItem__img"/>
+			<div className="AppItem__img-wrapper">
+				<img src={app.iconURL} alt={app.name} className="AppItem__img"/>
+			</div>
 			<div className="AppItem__info">
 				<h2>{app.name}</h2>
 				<div>{app.author}</div>
